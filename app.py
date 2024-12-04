44dfe2f3de91d36de7d4b8d8bf3cd649aedd4330
Ahzaaf.S <151323574+potato-pzy@users.noreply.github.com>
2024-12-04 22:20:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # app.py
+import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
